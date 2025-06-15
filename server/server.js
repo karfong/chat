1a6d804863a7cfee7ccaf14bec5c254d04a5c73f
Fong <chankarfong8888@gmail.com>
2025-06-15 23:06:08 +0800

    add user model

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
-import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
 
 
