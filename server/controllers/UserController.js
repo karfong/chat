35ff97561d4f193281700c868ac9095a848ea925
Fong <chankarfong8888@gmail.com>
2025-06-19 22:36:07 +0800

    add cloudinary file config and create the authentication middleware and usercontroller authentication route

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -55,3 +55,8 @@ export const login = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+//controller to check if user is authenticated
+export const checkAuth = async (req, res) => {
+    res.json({ success: true, user: req.user });
+}
