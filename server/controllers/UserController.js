22145a7a985c97a6aa6fe370139214548dcbdffc
Fong <chankarfong8888@gmail.com>
2025-06-20 23:00:58 +0800

    add api routes and add function for update profile

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js';
 //signup new user
 export const signup = async () => {
     const { fullName, email, password, bio } = req.body;
@@ -59,4 +60,27 @@ export const login = async (req, res) => {
 //controller to check if user is authenticated
 export const checkAuth = async (req, res) => {
     res.json({ success: true, user: req.user });
+}
+
+//controller to update user profile details
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic, bio, fullName } = req.body;
+        const userId = req.user._id;
+
+        let updatedUser;
+
+        if (!profilePic) {
+            updatedUser = await User.findByIdAndUpdate(userId, { bio, fullName }, { new: true })
+        } else {
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, { profilePic: upload.secure_url, bio, fullName }, { new: true });
+
+        }
+        res.json({ success: true, user: updatedUser })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
 }
