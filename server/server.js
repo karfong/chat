bc999ffb62ac61adc4a0295c2ed2f2f191f1a487
Fong <chankarfong8888@gmail.com>
2025-06-22 22:45:21 +0800

    add message api endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 
 //create express app and http server setup
@@ -17,6 +18,7 @@ app.use(cors());
 //route setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("api/messages", messageRouter);
 
 //connect to mongoDB
 await connectDB();
