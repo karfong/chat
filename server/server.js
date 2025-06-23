8dfce32809a3c446b806a16183c3e4a824d7c455
Fong <chankarfong8888@gmail.com>
2025-06-23 22:35:51 +0800

    add socket io setup and add senMessage funtion in message controller

--- server/server.js
+++ server/server.js
@@ -5,11 +5,38 @@ import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
-
+import { Server } from 'socket.io';
 
 //create express app and http server setup
 const app = express();
 const server = http.createServer(app);
+
+//socket.io setup
+export const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+})
+
+export const userSocketMap = {}; //{userId:socketId}
+
+//socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected: " + userId);
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+    }
+
+    //emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: " + userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
 
 //middleware
 app.use(express.json({ limit: "4mb" }));
