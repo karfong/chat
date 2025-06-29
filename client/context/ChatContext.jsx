b531a51e1f6760167171903ff54d358aa156eb20
Fong <chankarfong8888@gmail.com>
2025-06-29 22:11:02 +0800

    add chat context

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,7 +1,15 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
 
 export const ChatContext = createContext();
 export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [unsentMessages, setUnsentMessages] = useState({});
+  const { socket, axios } = useContext(AuthContext);
+
+  // function to get all users from sidebar
   const value = {};
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
